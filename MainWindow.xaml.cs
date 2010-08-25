e82e82f461ec4380bb6215ae89b36f444fef57b4
Tomas Linhart <SneakerXZ@gmail.com>
2010-08-25 22:39:29 +0200

    Initial version of Grooveshark for Desktop.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Navigation;
+using mshtml;
+
+namespace Grooveshark
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HideAdvertising()
+        {
+            //try
+            //{
+            //    browser.InvokeScript("hideAdvertisingBar");
+            //} 
+            //catch (Exception e)
+            //{
+            //    var msg = "Could not call script: " + e.Message;
+            //    MessageBox.Show(msg);
+            //}
+
+            if (browser.Document == null) return;
+
+            var document = browser.Document as IHTMLDocument2;
+            if (document == null) return;
+
+            document.parentWindow.execScript("javascript:(function(){document.getElementById('mainContainer').removeChild(document.getElementById('sidebar'));document.getElementById('mainContainer').insertBefore(document.getElementById('mainContent'),document.getElementById('mainContentWrapper'));})();");
+        }
+
+        private void BrowserLoadCompleted(object sender, NavigationEventArgs e)
+        {
+            HideAdvertising();
+        }
+    }
+}

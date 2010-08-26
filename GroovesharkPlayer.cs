d65d187c95d9e2c82f7498ad11bc8cd7e521632a
Tomas Linhart <SneakerXZ@gmail.com>
2010-08-26 03:03:11 +0200

    Added Thumbnail Toolbar for Taskbar with buttons for Previous Song, Play/Pause, Favorite, Next Song.

---
+++ GroovesharkPlayer.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows;
+using mshtml;
+
+namespace Grooveshark
+{
+    public class GroovesharkPlayer
+    {
+        private IHTMLDocument2 m_document;
+
+        public GroovesharkPlayer(IHTMLDocument2 document)
+        {
+            if (document == null)
+                throw new ArgumentNullException();
+
+            m_document = document;
+
+            AddWrappers();
+
+            HideAdvertising();
+        }
+
+        private void AddWrappers()
+        {
+            var head = m_document.all.tags("head")[0];
+            dynamic script = m_document.createElement("script");
+            script.type = "text/javascript";
+            script.text = @"var player = document.getElementById('gsliteswf');
+
+                            function nextWrapper() {
+                                player.next();
+                            }
+
+                            function previousWrapper() {
+                                player.previous();
+                            }
+
+                            function togglePlaybackWrapper() {
+                                player.togglePlayback();
+                            }
+
+                            function addSongsToQueueWrapper(param) {
+                                player.addSongsToQueue(param);
+                            }
+
+                            function pausePlaybackWrapper() {
+                                player.pausePlayback();
+                            }
+
+                            function playAlbumWrapper(param) {
+                                player.playAlbum(param);
+                            }
+
+                            function playPlaylistWrapper(param) {
+                                player.playPlaylist(param);
+                            }
+
+                            function setThemeWrapper(param) {
+                                player.setTheme(param);
+                            }
+
+                            function voteSongWrapper(param) {
+                                player.voteSong(param);
+                            }
+
+                            function favoriteSongWrapper() {
+                                player.favoriteSong();
+                            }
+
+                            function reloadUserLibraryWrapper() {
+                                player.reloadUserLibrary();
+                            }";
+
+            head.appendChild(script);
+        }
+
+        public void HideAdvertising()
+        {
+            ExecuteScript("hideAdvertisingBarWrapper();");
+        }
+
+        public void NextSong()
+        {
+            // also key ctrl + 39
+            ExecuteScript("nextWrapper();");
+        }
+
+        public void PreviousSong()
+        {
+            // also key ctrl + 37
+            ExecuteScript("previousWrapper();");
+        }
+
+        public void VoteSong(int rating)
+        {
+            ExecuteScript(String.Format("voteSongWrapper({0});", rating));
+        }
+
+        public void FavoriteSong()
+        {
+            ExecuteScript("favoriteSongWrapper();");
+        }
+
+        public void TogglePlayback()
+        {
+            // also key 32
+            ExecuteScript("togglePlaybackWrapper();");
+        }
+
+        public void PausePlayback()
+        {
+            ExecuteScript("pausePlaybackWrapper();");
+        }
+
+        public void SetTheme(int theme)
+        {
+            ExecuteScript(String.Format("voteSongWrapper({0});", theme));
+        }
+
+        public void PlayPlaylist(int playlist)
+        {
+            ExecuteScript(String.Format("playPlaylistWrapper({0});", playlist));
+        }
+
+        public void PlayAlbum(int album)
+        {
+            ExecuteScript(String.Format("playAlbumWrapper({0});", album));
+        }
+
+        public void VolumeUp()
+        {
+            // key ctrl + 38
+            throw new NotImplementedException();
+        }
+
+        public void VolumeDown()
+        {
+            // key ctrl + 40
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteScript(string script)
+        {
+            try 
+            {
+                m_document.parentWindow.execScript(script);
+            } 
+            catch (Exception e)
+            {
+#if DEBUG
+                MessageBox.Show("Could not call script: " + script +
+                    Environment.NewLine +
+                    "Exception: " + e.Message);
+#endif
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Navigation;
+﻿using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Navigation;
+using Microsoft.WindowsAPICodePack.Taskbar;
 using mshtml;
 
 namespace Grooveshark
@@ -8,34 +11,61 @@ namespace Grooveshark
     /// </summary>
     public partial class MainWindow
     {
+        private GroovesharkPlayer m_player;
+        private bool m_isPlaying;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void HideAdvertising()
-        {
-            //try
-            //{
-            //    browser.InvokeScript("hideAdvertisingBar");
-            //} 
-            //catch (Exception e)
-            //{
-            //    var msg = "Could not call script: " + e.Message;
-            //    MessageBox.Show(msg);
-            //}
-
-            if (browser.Document == null) return;
-
-            var document = browser.Document as IHTMLDocument2;
-            if (document == null) return;
-
-            document.parentWindow.execScript("javascript:(function(){document.getElementById('mainContainer').removeChild(document.getElementById('sidebar'));document.getElementById('mainContainer').insertBefore(document.getElementById('mainContent'),document.getElementById('mainContentWrapper'));})();");
-        }
-
         private void BrowserLoadCompleted(object sender, NavigationEventArgs e)
         {
-            HideAdvertising();
+            var document = browser.Document as IHTMLDocument2;
+            if (document == null) return;
+            
+            m_player = new GroovesharkPlayer(document);
+
+            CreateToolbarButtons();
+        }
+
+        private void CreateToolbarButtons()
+        {
+            var favorite = new ThumbnailToolbarButton(Properties.Resources.Favorite, "Add to Favorites");
+            favorite.Click += (sender, args) => m_player.FavoriteSong();
+
+            var next = new ThumbnailToolbarButton(Properties.Resources.Next, "Next Song");
+            next.Click += (sender, args) => m_player.NextSong();
+
+            var previous = new ThumbnailToolbarButton(Properties.Resources.Previous, "Previous Song");
+            previous.Click += (sender, args) => m_player.PreviousSong();
+
+            var playback = new ThumbnailToolbarButton(Properties.Resources.Play, "Play");
+
+            playback.Click += (sender, args) =>
+                                  {
+                                      m_player.TogglePlayback();
+
+                                      if (m_isPlaying)
+                                      {
+                                          m_isPlaying = false;
+                                          playback.Icon = Properties.Resources.Play;
+                                          playback.Tooltip = "Play";
+                                      }
+                                      else
+                                      {
+                                          m_isPlaying = true;
+                                          playback.Icon = Properties.Resources.Pause;
+                                          playback.Tooltip = "Pause";
+                                      }
+                                  };
+
+            TaskbarManager.Instance.ThumbnailToolbars.AddButtons(
+                new WindowInteropHelper(Application.Current.MainWindow).Handle,
+                previous,
+                playback,
+                favorite,
+                next);
         }
     }
 }
